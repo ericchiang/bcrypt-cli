47638af9b18954d0582438983eb5f53ce7931927
Eric Chiang <eric.chiang.m@gmail.com>
2016-11-14 16:30:39 -0800

    *: update bcrypt tool

--- bcrypt.go
+++ bcrypt.go
@@ -1,28 +1,62 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 
+	isatty "github.com/mattn/go-isatty"
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
+const Version = "not build correctly"
+
+func fatalf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+	os.Exit(2)
+}
+
+func getPassword(prompt string) ([]byte, error) {
+	fmt.Print(prompt + " ")
+	pw, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	return pw, err
+}
+
 func main() {
-	cost := flag.Int("cost", bcrypt.DefaultCost, "The bcrypt cost")
+	cost := flag.Int("cost", bcrypt.DefaultCost, "The bcrypt cost.")
+	version := flag.Bool("version", false, "Print the bcrypt version then exit.")
 	flag.Parse()
 
-	data, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "read stdin: %v", err)
-		os.Exit(2)
+	if *version {
+		fmt.Println(Version)
+		os.Exit(0)
 	}
 
-	out, err := bcrypt.GenerateFromPassword(data, *cost)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "bcrypt password: %v", err)
-		os.Exit(2)
+	if !isatty.IsTerminal(os.Stdout.Fd()) {
+		fatalf("this tools requires an interactive session\n")
 	}
-	os.Stdout.Write(out)
+
+	pw1, err := getPassword("Enter password:")
+	if err != nil {
+		fatalf("read password: %v\n", err)
+	}
+
+	pw2, err := getPassword("Re-enter password:")
+	if err != nil {
+		fatalf("read password: %v\n", err)
+	}
+
+	if !bytes.Equal(pw1, pw2) {
+		fatalf("passwords did not match\n")
+	}
+
+	out, err := bcrypt.GenerateFromPassword(pw1, *cost)
+	if err != nil {
+		fatalf("encrypting password: %v\n", err)
+	}
+
+	fmt.Printf("%s\n", out)
 }
